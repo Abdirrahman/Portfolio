ee605e6753c6edf08ee47973caff72275d830d33
abdirrahman <abdirrahmang@gmail.com>
2023-01-17 21:20:40 +0000

    fixed mq for mbp screen

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -39,7 +39,7 @@ export default function Projects() {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [screenWidth]);
 
   useEffect(() => {
     if (!toast.isActive(id)) {
